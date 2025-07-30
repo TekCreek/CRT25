eb8e04e8d18270eba82301e3a06bfe423c02f5e1
sagar <55848762+TekCreek@users.noreply.github.com>
2025-07-30 12:08:12 +0530

    CRUD operations added for RestEx

--- RestEx/sql/script.sql
+++ RestEx/sql/script.sql
@@ -16,4 +16,17 @@ CREATE TABLE `Product` (
 	price NUMERIC(10,2) NOT NULL
 );
 
+INSERT INTO `User`
+(username, password)
+VALUES('a', 'pwd');
+
+INSERT INTO `Product`` (name,price)
+VALUES
+ ('x',10.00),
+ ('y',20.00),
+ ('z',30.00),
+ ('ab',291.00),
+ ('ac',30.00),
+ ('ad',40.00),
+ ('pq',50.00);
 
--- RestEx/src/main/java/com/example/demo/api/ProductController.java
+++ RestEx/src/main/java/com/example/demo/api/ProductController.java
@@ -1,8 +1,12 @@
 package com.example.demo.api;
 
 import com.example.demo.entity.Product;
+import com.example.demo.model.ProductSearch;
 import com.example.demo.service.ProductService;
+import com.example.demo.service.ServiceLayerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,4 +26,30 @@ public class ProductController {
     public Product createProduct(@RequestBody Product product) {
         return productService.save(product);
     }
+
+    @PostMapping("/find")
+    public List<Product> findProducts(@RequestBody ProductSearch productSearch) {
+        return productService.findProducts(productSearch);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateProduct(@RequestBody Product product) {
+        try {
+            return ResponseEntity.ok(productService.update(product));
+        } catch (ServiceLayerException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable("id") long productId) {
+        try {
+            productService.delete(productId);
+            return ResponseEntity.ok("Product deleted");
+        } catch (ServiceLayerException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
 }
